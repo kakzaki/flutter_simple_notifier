46cf0cc911c5679d8e41d03678c96eb8769fa7ad
ZAKI MUBAROK <kakzaki@gmail.com>
2023-08-07 12:21:32 +0700

    add other test

---
+++ example/test/counter/counter_2_controller_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:simple_notifier_example/counter/counter_1_controller.dart';
+import 'package:simple_notifier_example/counter/counter_2_controller.dart';
+
+void main() {
+  // Test for Counter2Controller
+  test('Test Counter2Controller', () {
+    // Initialize the Counter2Controller
+    Counter2Controller.init();
+
+    // Test the increment method
+    Counter2Controller.increment();
+    expect(Counter2Controller.count.value, 1);
+
+    // Test the reset method
+    Counter2Controller.reset();
+    expect(Counter2Controller.count.value, 0);
+
+    // Dispose the Counter2Controller
+    Counter2Controller.dispose();
+  });
+}
--- example/test/counter/counter_3_controller_test.dart
+++ example/test/counter/counter_3_controller_test.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_notifier_example/counter/counter_1_controller.dart';
+import 'package:simple_notifier_example/counter/counter_3_controller.dart';
 
 void main() {
   // Test for Counter1Controller
   test('Test Counter1Controller', () {
+    final counterCon = Counter3Controller();
     // Test the increment method
-    Counter1Controller.increment();
-    expect(Counter1Controller.count.value, 1);
+    counterCon.increment();
+    expect(counterCon.count.value, 1);
 
     // Test the reset method
-    Counter1Controller.reset();
+    counterCon.reset();
     expect(Counter1Controller.count.value, 0);
   });
 }
