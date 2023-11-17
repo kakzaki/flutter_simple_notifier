177674d71c8f8be84687eaef4db03238360231c6
ZAKI MUBAROK <kakzaki@gmail.com>
2023-11-17 10:37:31 +0700

    format

--- test/src/multi_listen_extension_test.dart
+++ test/src/multi_listen_extension_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_notifier/simple_notifier.dart';
 
-
 void main() {
   testWidgets('Test MultiValueNotifierBuilder widget and listen extension',
       (WidgetTester tester) async {
